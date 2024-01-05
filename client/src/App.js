ad8c91a3b59ff82029bd76950fca79cb5af71160
Shibraj <shibrajdeb456@gmail.com>
2024-01-05 21:23:20 +0530

    added frontend to the projetct

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import Client from './components/Client';
+import Header from './components/Header';
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
+
+
+const client = new ApolloClient({
+  uri: 'http://localhost:5000/graphql',
+  cache: new InMemoryCache()
+});
+
+
+function App() {
+  return (
+    <>
+    <ApolloProvider client={client}>
+    <Header />
+    <div className="container">
+       <Client />
+    </div>
+    </ApolloProvider>
+    </>
+  );
+}
+
+export default App;
---
+++ client/src/components/Client.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Client = () => {
+  return (
+    <div>
+       Clients
+    </div>
+  )
+}
+
+export default Client
